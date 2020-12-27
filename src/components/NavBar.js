4bd53e1ff900c3e9b2d425d3012cebf69cff59a0
Jean de Dieu Uwimana <johnchafi@gmail.com>
2020-12-26 22:10:55 -0500

    debug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,94 +7,81 @@ import M from "materialize-css/dist/js/materialize.min.js";
 export default class Navbar extends Component {
   componentDidMount() {
     var elem = document.querySelector(".sidenav");
-    var instance = M.Sidenav.init(elem, {
+    M.Sidenav.init(elem, {
       edge: "left",
       inDuration: 250,
     });
   }
   render() {
     return (
-      <>
-        <nav className="light-blue darken-4">
-          <div className="container">
-            <div className="nav-wrapper">
-              <Link to="/" className="brand-logo">
-                Jean de Dieu Uwimana
-              </Link>
-              <Link to="/" data-target="side-nav" className="sidenav-trigger">
-                <i className="material-icons">menu</i>
-              </Link>
-              <ul className="right hide-on-med-and-down">
-                <li>
-                  <Link to="/">
-                    <i className="fas fa-home"></i> Home
-                  </Link>
-                </li>
-                <li>
-                  <Link to="skills">
-                    <i className="fas fa-copy"></i> Skills
-                  </Link>
-                </li>
-                <li>
-                  <Link to="experience">
-                    <i className="fas fa-id-badge"></i> Experiences
-                  </Link>
-                </li>
-                <li>
-                  <Link to="education">
-                    <i className="fas fa-graduation-cap"></i> Educations
-                  </Link>
-                </li>
-
-                <li>
-                  <Link2 to="portofolio" target="_blank">
-                    <i className="fas fa-address-card"></i> Portofolio
-                  </Link2>
-                </li>
-                {/* <li>*/}
-                {/*  <Link to="downloadCv">*/}
-                {/*    <i className="fas fa-copy"></i>*/}
-                {/*    Download My CV*/}
-                {/*  </Link>*/}
-                {/*</li>*/}
-              </ul>
+        <>
+          <nav className='light-blue darken-4'>
+            <div className='container'>
+              <div className='nav-wrapper'>
+                <Link to='/' className='brand-logo'>
+                  My CV
+                </Link>
+                <Link to='/' data-target='side-nav' className='sidenav-trigger'>
+                  <i className='material-icons'>menu</i>
+                </Link>
+                <ul className='right hide-on-med-and-down'>
+                  <li>
+                    <Link to='/'>
+                      <i class='fas fa-home'></i> Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='skills'>
+                      <i class='fas fa-copy'></i> Skills
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='experience'>
+                      <i class='fas fa-id-badge'></i> Experiences
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='education'>
+                      <i class='fas fa-graduation-cap'></i> Educations
+                    </Link>
+                  </li>
+                  <li>
+                    <Link2 to='portfolio'>
+                      <i class='fas fa-address-card'></i> Portfolios
+                    </Link2>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
-        <ul className="sticky-nav sidenav sidenav-close" id="side-nav">
-          <li />
-          <li>
-            <Link to="/">
-              <i className="fas fa-home"></i> Home
-            </Link>
-          </li>
-          <li>
-            <Link to="skills">
-              <i className="fas fa-copy"></i> Skills
-            </Link>
-          </li>
-          <li>
-            <Link to="experience">
-              <i className="fas fa-id-badge"></i> Experience
-            </Link>
-          </li>
-          <li>
-            <Link to="education">
-              <i className="fas fa-graduation-cap"></i> Education
-            </Link>
-          </li>
-          <li>
-            <Link2 to="portofolio">
-              <i className="fas fa-address-card"></i> Portofolio
-            </Link2>
-          </li>
-          {/* <li>*/}
-          {/*  <Link to="downloadCv">*/}
-          {/*    <i className="fas fa-graduation-cap"></i> Download My CV*/}
-          {/*  </Link>*/}
-          {/*</li>*/}
-        </ul>
-      </>
+          </nav>
+          <ul className='sidenav' id='side-nav'>
+            <li>
+              <Link to='/'>
+                <i class='fas fa-home'></i> Home
+              </Link>
+            </li>
+            <li>
+              <Link to='kills'>
+                <i class='fas fa-copy'></i> Skills
+              </Link>
+            </li>
+            <li>
+              <Link to='experience'>
+                <i class='fas fa-id-badge'></i> Experiences
+              </Link>
+            </li>
+            <li>
+              <Link to='education'>
+                <i class='fas fa-graduation-cap'></i> Educations
+              </Link>
+            </li>
+            <li>
+              <Link2 to='portfolio'>
+                <i class='fas fa-address-card'></i> Portfolios
+              </Link2>
+            </li>
+          </ul>
+        </>
     );
   }
 }
